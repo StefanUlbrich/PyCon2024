13f7647f1ca2fafcb599a9565c34f8d91dd07f9e
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2024-04-16 21:49:44 +0200

    Add Python skeleton

--- python/tests/test_gaussian_mixture.py
+++ python/tests/test_gaussian_mixture.py
@@ -1,2 +1,8 @@
-def test_hello() -> None:
+def test_expect() -> None:
+    # generate random mvn, sample data, check whether correct responsibilities
+    ...
+
+
+def test_maximize() -> None:
+    # call make blobs, see whether expect reconstructs the correct moments
     ...
