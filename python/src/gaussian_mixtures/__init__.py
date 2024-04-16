d9f07f9479a17c748bd3cc3807f29c5785f67808
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2024-04-16 21:49:44 +0200

    Add Python skeleton

--- python/src/gaussian_mixtures/__init__.py
+++ python/src/gaussian_mixtures/__init__.py
@@ -1,7 +1,39 @@
 """A very basic, educative implementation of Gaussian mixture models"""
 
+from dataclasses import dataclass
+from typing import NewType
+
+import numpy as np
+from numpy.typing import NDArray
+
 from . import gaussian_mixtures as bindings  # type: ignore  # noqa: F401
 
+Likelihood = NewType("Likelihood", NDArray[np.float64])
 
-def hello() -> None:
+
+@dataclass
+class GaussianMixtureModel:
+    """A mixture model"""
+
+    means: NDArray[np.float64]
+    covs: NDArray[np.float64]
+    weights: NDArray[np.float64]
+
+
+def initialize(data: NDArray[np.float64], n_components: int, alpha: float = 1.0) -> GaussianMixtureModel:
+    """Generate random responsibilities for intialization"""
+    raise NotImplementedError()
+
+
+def expect(gmm: GaussianMixtureModel, data: NDArray[np.float64]) -> Likelihood:
+    """Expectation step"""
+    raise NotImplementedError()
+
+
+def maximize(gmm: GaussianMixtureModel, responsibilities: Likelihood, data: NDArray[np.float64]) -> None:
+    """Maximization step"""
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
     ...
