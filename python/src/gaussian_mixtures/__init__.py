fb9c5d8f41b208124fa7a917b7611682a82f50d5
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2024-04-17 00:18:18 +0200

    Add implementation

--- python/src/gaussian_mixtures/__init__.py
+++ python/src/gaussian_mixtures/__init__.py
@@ -5,6 +5,7 @@ from typing import NewType
 
 import numpy as np
 from numpy.typing import NDArray
+from scipy.stats import multivariate_normal as mvn
 
 from . import gaussian_mixtures as bindings  # type: ignore  # noqa: F401
 
@@ -22,17 +23,43 @@ class GaussianMixtureModel:
 
 def initialize(data: NDArray[np.float64], n_components: int, alpha: float = 1.0) -> GaussianMixtureModel:
     """Generate random responsibilities for intialization"""
-    raise NotImplementedError()
+
+    responsibilities: Likelihood = Likelihood(np.random.dirichlet(n_components * [alpha], data.shape[0]))
+
+    gmm = GaussianMixtureModel(np.zeros(0), np.zeros(0), np.zeros(0))
+    maximize(gmm, responsibilities, data)
+    return gmm
 
 
 def expect(gmm: GaussianMixtureModel, data: NDArray[np.float64]) -> Likelihood:
-    """Expectation step"""
-    raise NotImplementedError()
+    """Expectation step."""
+
+    res = np.array([w * mvn.pdf(data, mean=m, cov=c) for m, c, w in zip(gmm.means, gmm.covs, gmm.weights)])
+    res /= res.sum(axis=0)
+    return Likelihood(res.T)
+    # return Likelihood(res)
 
 
 def maximize(gmm: GaussianMixtureModel, responsibilities: Likelihood, data: NDArray[np.float64]) -> None:
     """Maximization step"""
-    raise NotImplementedError()
+
+    assert responsibilities.shape[0] == data.shape[0]
+
+    sum_responsibilities = responsibilities.sum(axis=0)
+
+    gmm.means = (
+        np.sum(data[:, np.newaxis, :] * responsibilities[:, :, np.newaxis], axis=0)
+        / sum_responsibilities[:, np.newaxis]
+    )
+
+    data = data[:, np.newaxis, :] - gmm.means[np.newaxis, :, :]
+
+    gmm.covs = (
+        np.einsum("nkd, nk,  nke -> kde", data, responsibilities, data)
+        / sum_responsibilities[:, np.newaxis, np.newaxis]
+    )
+
+    gmm.weights = sum_responsibilities / sum_responsibilities.sum()
 
 
 if __name__ == "__main__":
